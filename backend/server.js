4779a90ef35692bf67b526c99f14440cd47078bc
Umair452 <arshadu452@gmail.com>
2024-08-28 05:28:48 +0500

    basic setup of express

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+const express = require('express')
+
+
+//express app
+const app = express()
+
+//middleware
+app.use((req, res, next)=>{
+    console.log(req.path, req.method);
+    next()
+    
+})
+
+//routes
+app.get('/', (req, res)=>{
+    res.json({mssg: 'Welcome to the app'})
+})
+
+app.listen(process.env.PORT, ()=>{
+    console.log('Listening to port!!!');
+    
+})
+
